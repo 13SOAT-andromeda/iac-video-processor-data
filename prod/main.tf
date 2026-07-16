ba55412536ad50f946c91ab42dc062092a3849e6
JulioCVaz <julio.oliveiravaz0@gmail.com>
2026-07-15 23:38:17 -0300

    chore: bootstrap prod/ terraform skeleton (backend, provider, variables)

---
+++ prod/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.11"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.54"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "video-processor-bucket-andromeda"
+    key          = "video-processor-data/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Terraform   = "true"
+      Environment = var.environment
+      Project     = "video-processor"
+    }
+  }
+}
